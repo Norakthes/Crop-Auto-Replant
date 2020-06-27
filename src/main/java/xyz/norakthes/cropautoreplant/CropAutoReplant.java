dcff28644c372f2be570bbc6e80ad0f78112a6ae
Rasmus Bødker <rasmus.b.bodker@gmail.com>
2020-06-28 00:28:52 +0200

    I'm stupid and have finally realized what I should have done in the first place

--- src/main/java/xyz/norakthes/cropautoreplant/CropAutoReplant.java
+++ src/main/java/xyz/norakthes/cropautoreplant/CropAutoReplant.java
@@ -11,8 +11,6 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Objects;
-
 public final class CropAutoReplant extends JavaPlugin implements Listener {
 
     @Override
@@ -27,34 +25,35 @@ public final class CropAutoReplant extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
-        String world = event.getPlayer().getWorld().getName();
-        Location location = Objects.requireNonNull(event.getClickedBlock()).getLocation();
-        Material block = event.getClickedBlock().getType();
-        String blockData = event.getClickedBlock().getBlockData().getAsString();
-        boolean interaction = event.getAction().equals(Action.RIGHT_CLICK_BLOCK);
-        if (interaction && block.name().equals("WHEAT") && blockData.equals("minecraft:wheat[age=7]")){
-            event.getClickedBlock().setType(Material.AIR);
-            Bukkit.getWorld(world).dropItemNaturally(location, new ItemStack(Material.WHEAT));
-            location.getWorld().getBlockAt(location).setType(Material.WHEAT);
-            Bukkit.getWorld(world).spawnParticle(Particle.BLOCK_CRACK, location.add(0.5,0,0.5), 200, block.createBlockData());
-        }
-        if (interaction && block.name().equals("CARROTS") && blockData.equals("minecraft:carrots[age=7]")){
-            event.getClickedBlock().setType(Material.AIR);
-            Bukkit.getWorld(world).dropItemNaturally(location, new ItemStack(Material.CARROT));
-            location.getWorld().getBlockAt(location).setType(Material.CARROTS);
-            Bukkit.getWorld(world).spawnParticle(Particle.BLOCK_CRACK, location.add(0.5,0,0.5), 200, block.createBlockData());
-        }
-        if (interaction && block.name().equals("POTATOES") && blockData.equals("minecraft:potatoes[age=7]")){
-            event.getClickedBlock().setType(Material.AIR);
-            Bukkit.getWorld(world).dropItemNaturally(location, new ItemStack(Material.POTATO));
-            location.getWorld().getBlockAt(location).setType(Material.POTATOES);
-            Bukkit.getWorld(world).spawnParticle(Particle.BLOCK_CRACK, location.add(0.5,0,0.5), 200, block.createBlockData());
-        }
-        if (interaction && block.name().equals("BEETROOTS") && blockData.equals("minecraft:beetroots[age=3]")) {
-            event.getClickedBlock().setType(Material.AIR);
-            Bukkit.getWorld(world).dropItemNaturally(location, new ItemStack(Material.BEETROOT));
-            location.getWorld().getBlockAt(location).setType(Material.BEETROOTS);
-            Bukkit.getWorld(world).spawnParticle(Particle.BLOCK_CRACK, location.add(0.5, 0, 0.5), 200, block.createBlockData());
+        if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+            String world = event.getPlayer().getWorld().getName();
+            Location location = event.getClickedBlock().getLocation();
+            Material block = event.getClickedBlock().getType();
+            String blockData = event.getClickedBlock().getBlockData().getAsString();
+            if (block.name().equals("WHEAT") && blockData.equals("minecraft:wheat[age=7]")) {
+                event.getClickedBlock().setType(Material.AIR);
+                Bukkit.getWorld(world).dropItemNaturally(location, new ItemStack(Material.WHEAT));
+                location.getWorld().getBlockAt(location).setType(Material.WHEAT);
+                Bukkit.getWorld(world).spawnParticle(Particle.BLOCK_CRACK, location.add(0.5, 0, 0.5), 200, block.createBlockData());
+            }
+            if (block.name().equals("CARROTS") && blockData.equals("minecraft:carrots[age=7]")) {
+                event.getClickedBlock().setType(Material.AIR);
+                Bukkit.getWorld(world).dropItemNaturally(location, new ItemStack(Material.CARROT));
+                location.getWorld().getBlockAt(location).setType(Material.CARROTS);
+                Bukkit.getWorld(world).spawnParticle(Particle.BLOCK_CRACK, location.add(0.5, 0, 0.5), 200, block.createBlockData());
+            }
+            if (block.name().equals("POTATOES") && blockData.equals("minecraft:potatoes[age=7]")) {
+                event.getClickedBlock().setType(Material.AIR);
+                Bukkit.getWorld(world).dropItemNaturally(location, new ItemStack(Material.POTATO));
+                location.getWorld().getBlockAt(location).setType(Material.POTATOES);
+                Bukkit.getWorld(world).spawnParticle(Particle.BLOCK_CRACK, location.add(0.5, 0, 0.5), 200, block.createBlockData());
+            }
+            if (block.name().equals("BEETROOTS") && blockData.equals("minecraft:beetroots[age=3]")) {
+                event.getClickedBlock().setType(Material.AIR);
+                Bukkit.getWorld(world).dropItemNaturally(location, new ItemStack(Material.BEETROOT));
+                location.getWorld().getBlockAt(location).setType(Material.BEETROOTS);
+                Bukkit.getWorld(world).spawnParticle(Particle.BLOCK_CRACK, location.add(0.5, 0, 0.5), 200, block.createBlockData());
+            }
         }
     }
 }
