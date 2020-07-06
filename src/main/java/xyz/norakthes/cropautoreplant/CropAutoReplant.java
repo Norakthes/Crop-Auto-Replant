a3c6c56414e70e479bbd274cfcc2e55253b3e5eb
Rasmus Bødker <rasmus.b.bodker@gmail.com>
2020-07-06 19:21:38 +0200

    Drops the blocks drops, instead of manually putting in specified drops

--- src/main/java/xyz/norakthes/cropautoreplant/CropAutoReplant.java
+++ src/main/java/xyz/norakthes/cropautoreplant/CropAutoReplant.java
@@ -8,7 +8,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CropAutoReplant extends JavaPlugin implements Listener {
@@ -31,26 +30,22 @@ public final class CropAutoReplant extends JavaPlugin implements Listener {
             Material block = event.getClickedBlock().getType();
             String blockData = event.getClickedBlock().getBlockData().getAsString();
             if (block.name().equals("WHEAT") && blockData.equals("minecraft:wheat[age=7]")) {
-                event.getClickedBlock().setType(Material.AIR);
-                Bukkit.getWorld(world).dropItemNaturally(location, new ItemStack(Material.WHEAT));
+                event.getClickedBlock().breakNaturally();
                 location.getWorld().getBlockAt(location).setType(Material.WHEAT);
                 Bukkit.getWorld(world).spawnParticle(Particle.BLOCK_CRACK, location.add(0.5, 0, 0.5), 200, block.createBlockData());
             }
             if (block.name().equals("CARROTS") && blockData.equals("minecraft:carrots[age=7]")) {
-                event.getClickedBlock().setType(Material.AIR);
-                Bukkit.getWorld(world).dropItemNaturally(location, new ItemStack(Material.CARROT));
+                event.getClickedBlock().breakNaturally();
                 location.getWorld().getBlockAt(location).setType(Material.CARROTS);
                 Bukkit.getWorld(world).spawnParticle(Particle.BLOCK_CRACK, location.add(0.5, 0, 0.5), 200, block.createBlockData());
             }
             if (block.name().equals("POTATOES") && blockData.equals("minecraft:potatoes[age=7]")) {
-                event.getClickedBlock().setType(Material.AIR);
-                Bukkit.getWorld(world).dropItemNaturally(location, new ItemStack(Material.POTATO));
+                event.getClickedBlock().breakNaturally();
                 location.getWorld().getBlockAt(location).setType(Material.POTATOES);
                 Bukkit.getWorld(world).spawnParticle(Particle.BLOCK_CRACK, location.add(0.5, 0, 0.5), 200, block.createBlockData());
             }
             if (block.name().equals("BEETROOTS") && blockData.equals("minecraft:beetroots[age=3]")) {
-                event.getClickedBlock().setType(Material.AIR);
-                Bukkit.getWorld(world).dropItemNaturally(location, new ItemStack(Material.BEETROOT));
+                event.getClickedBlock().breakNaturally();
                 location.getWorld().getBlockAt(location).setType(Material.BEETROOTS);
                 Bukkit.getWorld(world).spawnParticle(Particle.BLOCK_CRACK, location.add(0.5, 0, 0.5), 200, block.createBlockData());
             }
